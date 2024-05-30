f259c956dbaff46160aec2f88460cad2b92b9f77
Keya Hu <88616362+lillian039@users.noreply.github.com>
2024-05-30 13:18:22 +0800

    load gsm8k

--- src/load_data.py
+++ src/load_data.py
@@ -1,8 +1,16 @@
-from datasets import load_dataset,list_datasets
 
-def load_gsm8k_dataset():
-    print(list_datasets())
-    dataset = load_dataset('gsm8k')
-    return dataset
+import json
+def load_jsonl(file_path):
+    data = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            data.append(json.loads(line))
+    return data
 
-load_gsm8k_dataset()
+def load_gsm_data():
+    file_path = '../data/grade_school_math/data/'
+    train_data = load_jsonl(file_path + 'train.jsonl')
+    test_data = load_jsonl(file_path + 'test.jsonl')
+    train_socratic_data = load_jsonl(file_path + 'train_socratic.jsonl')
+    test_socratic_data = load_jsonl(file_path + 'test_socratic.jsonl')
+    return train_data, test_data, train_socratic_data, test_socratic_data
